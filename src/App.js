eafff2b741de7f61216d38abffbb8d51c0353109
rahul kathait <edendattox@gmail.com>
2022-01-26 18:59:11 +0530

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
-import { ResourceLoader } from "./ResourceLoader";
-import { UserInfo } from "./UserInfo";
-import { ProductInfo } from "./ProductInfo";
-import axios from "axios";
-import { DataSource } from "./DataSource";
+// import { ResourceLoader } from "./ResourceLoader";
+// import { UserInfo } from "./UserInfo";
+// import { ProductInfo } from "./ProductInfo";
+// import axios from "axios";
+// import { DataSource } from "./DataSource";
+// import { printProps } from "./printProps";
+// import { withUser } from "./withUser";
 
-const getServerData = (url) => async () => {
-  const { data } = await axios.get(url);
-  return data;
-};
+// const UserInfoWrapped = printProps(UserInfo);
+// const UserInfoWithLoader = withUser(UserInfo, "123");
 
-const getLocalStorageData = (key) => () => {
-  return localStorage.getItem(key);
-};
+import { UserInfoForm } from "./UserInfoForm";
 
-const Text = ({ message }) => <h1>{message}</h1>;
-
-function App() {
+export function App() {
   return (
     <>
-      <ResourceLoader resourceUrl="/users/123" resourceName="user">
-        <UserInfo />
-      </ResourceLoader>
-      <ResourceLoader resourceUrl="/products/101" resourceName="product">
-        <ProductInfo />
-      </ResourceLoader>
-      <DataSource getDataFunc={getServerData("/users/123")} resourceName="user">
-        <UserInfo />
-      </DataSource>
-      <DataSource
-        getDataFunc={getLocalStorageData("message")}
-        resourceName="message"
-      >
-        <Text />
-      </DataSource>
+      <UserInfoForm />
     </>
   );
 }
 
-export default App;
+// const getServerData = (url) => async () => {
+//   const { data } = await axios.get(url);
+//   return data;
+// };
+
+// const getLocalStorageData = (key) => () => {
+//   return localStorage.getItem(key);
+// };
+
+// const Text = ({ message }) => <h1>{message}</h1>;
+
+// function App() {
+//   return (
+//     <>
+//     <printProps />
+//       <ResourceLoader resourceUrl="/users/123" resourceName="user">
+//         <UserInfo />
+//       </ResourceLoader>
+//       <ResourceLoader resourceUrl="/products/101" resourceName="product">
+//         <ProductInfo />
+//       </ResourceLoader>
+//       <DataSource getDataFunc={getServerData("/users/123")} resourceName="user">
+//         <UserInfo />
+//       </DataSource>
+//       <DataSource
+//         getDataFunc={getLocalStorageData("message")}
+//         resourceName="message"
+//       >
+//         <Text />
+//       </DataSource>
+//     </>
+//   );
+// }
+
+// export default App;
